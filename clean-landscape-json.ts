b8ee5cfcf2e13b45ccf82499d6d5aa111151c4f8
Matt Young <halcyondude@gmail.com>
2022-04-09 21:10:25 -0400

    GitHub Flat: add Deno post-processing script
    the .member and .relation fields of each card have values (perhaps?)
    used by the landscape app, however when importing via Cypher we end
    up with Member type of "false", as well as Relation types of "member"
    and "false" - as well as thousands of relationships.
    
    This commit uses the flat action's postprocessing step to remove them
    from the input data.

---
+++ clean-landscape-json.ts
@@ -0,0 +1,37 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
+import * as path from "https://deno.land/std/path/mod.ts";
+
+const filename = Deno.args[0]; // Same name as downloaded_filename
+
+// read file downloaded by flat (landscape-items.json)
+const landscape = await readJSON(filename);
+
+for (let i = 0; i < landscape.length; i++) 
+{
+    let card = landscape[i];
+
+    //
+    // MEMBER := { false, Platinum, Nonprofit, Silver, Gold, End User Supporter, Academic }
+    // strip 'false'
+    //
+    if( card['member'] === false ) {
+        delete card['member'];
+    }
+
+    //
+    // RELATION := { false, sandbox, member, incubating, graduated, archived }
+    // strip false and member, as both are duplicative.
+    //
+    let val = card['relation'];
+    if( false === val || 'member' === val ) {
+        delete card['relation'];
+    }
+}
+
+let p = path.parse(filename);
+p.name = 'clean-' + p.name;
+const newfile = p.dir + '/' + p.name + p.ext
+
+console.log('Writing cleaned up landscape file: ' + newfile)
+
+await writeJSON(newfile, landscape, null, 2)
