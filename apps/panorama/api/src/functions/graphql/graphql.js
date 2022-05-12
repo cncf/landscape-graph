20d7ed52bd0d2d26bd5ce47f24c4b95cf2a2a85e
Matt Young <halcyondude@gmail.com>
2022-05-12 11:27:01 -0400

    apps/panorama: initial project creation
    Signed-off-by: Matt Young <halcyondude@gmail.com>

---
+++ apps/panorama/api/src/functions/graphql/graphql.js
@@ -0,0 +1,27 @@
+// This module can be used to serve the GraphQL endpoint
+// as a lambda function
+
+const { ApolloServer } = require('apollo-server-lambda')
+import { Neo4jGraphQL } from '@neo4j/graphql'
+const neo4j = require('neo4j-driver')
+
+// This module is copied during the build step
+// Be sure to run `npm run build`
+const { typeDefs } = require('./graphql-schema')
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI || 'bolt://localhost:7687',
+  neo4j.auth.basic(
+    process.env.NEO4J_USER || 'neo4j',
+    process.env.NEO4J_PASSWORD || 'neo4j'
+  )
+)
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+
+const server = new ApolloServer({
+  schema: neoSchema.schema,
+  context: { driver, neo4jDatabase: process.env.NEO4J_DATABASE },
+})
+
+exports.handler = server.createHandler()
